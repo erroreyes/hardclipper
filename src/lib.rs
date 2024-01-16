77742225bbfca9c179d1f7d221176f80797f8d81
erroreyes <error.eyes@proton.me>
2024-01-15 16:52:43 -0800

    refactored the clipper process into a function, and minimized its complextity

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,8 @@
-use nih_plug::prelude::*;
+use nih_plug::{prelude::*, log::{log, Level}};
 use std::sync::Arc;
 
 struct Hardclipper {
     params: Arc<HardclipperParams>,
-}
-
-#[derive(Params)]
-struct HardclipperParams {
-    #[id = "input_gain"]
-    pub input_gain: FloatParam,
-    #[id = "ceiling"]
-    pub ceiling: FloatParam,
-    #[id = "reduce"]
-    pub reduce: FloatParam,
-    #[id = "output_gain"]
-    pub output_gain: FloatParam,
-    #[id = "delta"]
-    pub delta: BoolParam,
 }
 
 impl Default for Hardclipper {
@@ -27,9 +13,57 @@ impl Default for Hardclipper {
     }
 }
 
+/// Hardclipper implementation with the actual clipping function.
+impl Hardclipper {
+    /// Clip the given sample and return the result.
+    fn clip(&self, sample: f32) -> f32 {
+        let input_gain = self.params.input_gain.smoothed.next();
+        let ceiling = self.params.ceiling.smoothed.next();
+        let reduce = self.params.reduce.smoothed.next();
+        let output_gain = self.params.output_gain.smoothed.next();
+        let delta = self.params.delta.value();
+
+        // Adjust the input gain.
+        let input = sample * input_gain;
+                
+        // Clip audio above the threshold.
+        let output = if input.abs() > ceiling {
+            input.signum() * ceiling * reduce
+        } else {
+            input
+        };
+
+        // Return clipped audio, or the delta if it's on
+        output_gain * if delta { 
+            sample - output 
+        } else { 
+            output 
+        }
+    }
+}
+
+#[derive(Params)]
+struct HardclipperParams {
+    #[id = "input_gain"]
+    pub input_gain: FloatParam,
+    
+    #[id = "ceiling"]
+    pub ceiling: FloatParam,
+    
+    #[id = "reduce"]
+    pub reduce: FloatParam,
+    
+    #[id = "output_gain"]
+    pub output_gain: FloatParam,
+    
+    #[id = "delta"]
+    pub delta: BoolParam,
+}
+
 impl Default for HardclipperParams {
     fn default() -> Self {
         Self {
+            // ---------------------------------------------------------------- Input
             input_gain: FloatParam::new(
                 "Input",
                 util::db_to_gain(0.0),
@@ -43,7 +77,8 @@ impl Default for HardclipperParams {
             .with_unit(" dB")
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
-
+            
+            // ---------------------------------------------------------------- Celing
             ceiling: FloatParam::new(
                 "Ceiling",
                 util::db_to_gain(0.0),
@@ -57,7 +92,8 @@ impl Default for HardclipperParams {
             .with_unit(" dB")
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
-
+            
+            // ---------------------------------------------------------------- Reduce
             reduce: FloatParam::new(
                 "Reduce",
                 1.0,
@@ -70,7 +106,8 @@ impl Default for HardclipperParams {
             .with_unit(" %")
             .with_value_to_string(formatters::v2s_f32_percentage(0))
             .with_string_to_value(formatters::s2v_f32_percentage()),
-
+            
+            // ---------------------------------------------------------------- Output
             output_gain: FloatParam::new(
                 "Output",
                 util::db_to_gain(0.0),
@@ -84,7 +121,8 @@ impl Default for HardclipperParams {
             .with_unit(" dB")
             .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
             .with_string_to_value(formatters::s2v_f32_gain_to_db()),
-
+            
+            // ---------------------------------------------------------------- Delta
             delta: BoolParam::new(
                 "Delta", 
                 false
@@ -133,12 +171,14 @@ impl Plugin for Hardclipper {
         // Resize buffers and perform other potentially expensive initialization operations here.
         // The `reset()` function is always called right after this function. You can remove this
         // function if you do not need it.
+        log!(Level::Info, "hardclipper initialized.");
         true
     }
 
     fn reset(&mut self) {
         // Reset buffers and envelopes here. This can be called from the audio thread and may not
         // allocate. You can remove this function if you do not need it.
+        log!(Level::Info, "hardclipper reset.")
     }
 
     fn process(
@@ -147,33 +187,12 @@ impl Plugin for Hardclipper {
         _aux: &mut AuxiliaryBuffers,
         _context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
+                
         for channel_samples in buffer.iter_samples() {
-            let input_gain = self.params.input_gain.smoothed.next();
-            let ceiling = self.params.ceiling.smoothed.next();
-            let reduce = self.params.reduce.smoothed.next();
-            let output_gain = self.params.output_gain.smoothed.next();
 
             for sample in channel_samples {
-                // Increase audio by input gain
-                *sample *= input_gain;
-                
-                // If the sample is above the +/- ceiling, return the +/- ceiling value
-                // otherwise return the sample value
-                let output = if *sample > ceiling {
-                    ceiling * reduce
-                } else if *sample < -ceiling {
-                    -ceiling * reduce
-                } else {
-                    *sample
-                };
 
-                // If delta is on, return the difference between original audio and the clipped audio, 
-                // otherwise return the clipped audio
-                *sample = output_gain * if self.params.delta.value() { 
-                    *sample - output 
-                } else { 
-                    output 
-                };
+                *sample = self.clip(*sample);
             }
         }
 
